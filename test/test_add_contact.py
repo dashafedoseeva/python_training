0bb3511b9e142cf12de332cbf5deb8f9db7434d9
Daria.Fedoseeva <daria.fedoseeva@billing.ru>
2018-10-01 01:24:51 +0300

    Задание №5: Реализовать двухуровневую архитектуру тестов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import pytest
 
-from contact import Contact
 from fixture.application import Application
+from model.contact import Contact
 
 
 @pytest.fixture
@@ -15,14 +15,13 @@ def app(request):
 def test_add_contact(app):
     app.open_home_page()
     app.session.login(username="admin", password="secret")
-    app.open_contact_page()
-    app.create_contact(Contact(firstname="fdfd", middlename="dd", lastname="ssd", nickname="dfd", title="fd",
-                                    company="gg", address="dds", home="sfs", mobile="sdsd", work="sdfd", fax="tt",
-                                    email="sffs",
-                                    email2="sdff", email3="s", homepage="dsfdsfd", bday="1", bmonth="February",
-                                    byear="44", aday="2", amonth="February", ayear="4545", address2="gergt",
-                                    phone2="ergth",
-                                    notes="trt"))
-    app.return_to_home_page()
+    app.contact.open_contact_page()
+    app.contact.create_contact(Contact(firstname="fdfd", middlename="dd", lastname="ssd", nickname="dfd", title="fd",
+                                       company="gg", address="dds", home="sfs", mobile="sdsd", work="sdfd", fax="tt",
+                                       email="sffs",
+                                       email2="sdff", email3="s", homepage="dsfdsfd", bday="1", bmonth="February",
+                                       byear="44", aday="2", amonth="February", ayear="4545", address2="gergt",
+                                       phone2="ergth",
+                                       notes="trt"))
+    app.contact.return_to_home_page()
     app.session.logout()
-
