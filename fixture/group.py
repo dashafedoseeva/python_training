0e790a8bb32f8d40702262a6478c3665f1478046
Daria.Fedoseeva <daria.fedoseeva@billing.ru>
2018-10-09 01:37:03 +0300

    Добавлены изменения в тесты удаления групп и контактов

--- fixture/group.py
+++ fixture/group.py
@@ -26,7 +26,6 @@ class GroupHelper:
         self.change_field_value('group_header', group.header)
         self.change_field_value('group_footer', group.footer)
 
-
     def change_field_value(self, field_name, text):
         wd = self.app.wd
         if text is not None:
@@ -56,15 +55,19 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         self.select_first_group()
-        #open modification form
+        # open modification form
         wd.find_element_by_name("edit").click()
-        #fill group form
+        # fill group form
         self.fill_group_form(new_group_data)
-        #submit modification
+        # submit modification
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
-
 
     def return_to_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
+
+    def count(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        return len(wd.find_elements_by_name("selected[]"))
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,4 +1,16 @@
 # -*- coding: utf-8 -*-
+from model.contact import Contact
+
+
 def test_del_contact(app):
-    app.open_home_page()
+    if app.contact.count_contact() == 0:
+        app.contact.open_contact_page()
+        app.contact.create_contact(
+            Contact(firstname="fdfd", middlename="dd", lastname="ssd", nickname="dfd", title="fd",
+                    company="gg", address="dds", home="sfs", mobile="sdsd", work="sdfd", fax="tt",
+                    email="sffs",
+                    email2="sdff", email3="s", homepage="dsfdsfd", bday="1", bmonth="February",
+                    byear="44", aday="2", amonth="February", ayear="4545", address2="gergt",
+                    phone2="ergth",
+                    notes="trt"))
     app.contact.delete_first_contact()
