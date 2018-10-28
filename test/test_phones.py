d773e23b9fb72c89d85df9e9cf368bb579972e96
Daria.Fedoseeva <daria.fedoseeva@billing.ru>
2018-10-29 01:41:44 +0300

    сравниваются все элементы с главной страницы и со страницы редактирования

--- test/test_phones.py
+++ test/test_phones.py
@@ -1,7 +1,7 @@
 import re
 
 
-def test_phones_on_home_page(app):
+def test_all_info_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
