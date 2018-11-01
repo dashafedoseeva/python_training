c15fe821fe79bb20d240bf5bac7770d328ebfebc
Daria.Fedoseeva <daria.fedoseeva@billing.ru>
2018-11-02 01:10:33 +0300

    Задание №16: Реализовать опции командной строки, позволяющие указывать тип браузера и адрес тестируемого приложения

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -15,10 +15,10 @@ def test_del_some_contact(app):
                           phone2="ergth",
                           notes="trt")
         app.contact.create_contact(contact)
-    old_contacts = app.contact.get_contact_list
+    old_contacts = app.contact.get_contact_list()
     index = randrange(len(old_contacts))
     app.contact.delete_contact_by_index(index)
-    new_contacts = app.contact.get_contact_list
+    new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) - 1 == len(new_contacts)
-    old_contacts[index:index+1] = []
+    old_contacts[index:index + 1] = []
     assert old_contacts == new_contacts
