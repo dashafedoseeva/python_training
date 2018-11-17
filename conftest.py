084a2c726f541c0ce939759a19aef7a4c97317bb
Daria.Fedoseeva <daria.fedoseeva@billing.ru>
2018-11-18 01:32:01 +0300

    все работает

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import importlib
 import json
 
 import os
@@ -9,17 +10,18 @@ from fixture.application import Application
 fixture = None
 target = None
 
+
 @pytest.fixture
 def app(request):
     global fixture
     global target
-    browser=request.config.getoption("--browser")
+    browser = request.config.getoption("--browser")
     if target is None:
-        config_file= os.path.join(os.path.dirname(os.path.abspath(__file__)),request.config.getoption("--target"))
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
         with open(config_file) as f:
             target = json.load(f)
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url= target['baseUrl'])
+        fixture = Application(browser=browser, base_url=target['baseUrl'])
     fixture.session.ensure_login(username=target["username"], password=target["password"])
     return fixture
 
@@ -38,3 +40,13 @@ def pytest_addoption(parser):
     parser.addoption("--browser", action='store', default='chrome')
     parser.addoption("--target", action='store', default="target.json")
 
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_form_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+
+def load_form_module(module):
+    return importlib.import_module("data.%s" % module).testdata
